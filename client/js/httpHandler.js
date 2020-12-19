cbc2c7030eea4029a69e0718f14aea5b96af1223
Ken Medbery <kennethmedbery@gmail.com>
2020-12-18 21:11:26 -0600

    Complete connecting client to server

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,10 +1,29 @@
-(function() {
+(function () {
 
   const serverUrl = 'http://127.0.0.1:3000';
 
   //
   // TODO: build the swim command fetcher here
   //
+
+
+  const randomCommand = () => {
+    $.ajax({
+      url: serverUrl,
+      // data: data,
+      success: (command) => {
+        console.log(command);
+        SwimTeam.move(command);
+
+        setTimeout(randomCommand, 2000);
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  randomCommand();
+
+
 
   /////////////////////////////////////////////////////////////////////
   // The ajax file uplaoder is provided for your convenience!
@@ -17,7 +36,7 @@
     $.ajax({
       type: 'POST',
       data: formData,
-      url: serverUrl;
+      url: serverUrl,
       cache: false,
       contentType: false,
       processData: false,
@@ -28,7 +47,7 @@
     });
   };
 
-  $('form').on('submit', function(e) {
+  $('form').on('submit', function (e) {
     e.preventDefault();
 
     var form = $('form .file')[0];
